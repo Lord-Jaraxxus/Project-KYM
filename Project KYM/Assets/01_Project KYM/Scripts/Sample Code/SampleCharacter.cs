969417b9fad4a8e1fbfef8679268b9304b0171b8
Lord-Jaraxxus <ym970812@naver.com>
2025-05-28 21:14:24 +0900

    Second Lesson Assignment

--- Project KYM/Assets/01_Project KYM/Scripts/Sample Code/SampleCharacter.cs
+++ Project KYM/Assets/01_Project KYM/Scripts/Sample Code/SampleCharacter.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
 using UnityEngine;
 
 public class SampleCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int hp;
+    int maxHp;
+    int money;
+    float timer;
+    bool isPoison;
+
+    void Awake()
     {
-        
+        maxHp = 100;
+        hp = 100;
+        money = 0;
+        timer = 0;
+        isPoison = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.name == "Red Cube") { hp -= 30; }
+        else if (other.gameObject.name == "Blue Cube") { hp = maxHp; }
+        else if (other.gameObject.name == "Green Cube") { isPoison = true; }
+
+        UnityEngine.Debug.Log($"HP : {hp}");
     }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (isPoison)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 0.5f)
+            {
+                hp -= 10;
+                UnityEngine.Debug.Log($"HP : {hp}");
+                timer = 0;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Green Cube") 
+        { 
+            isPoison = false; 
+            timer = 0;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ITEM_BOX"))
+        {
+            Destroy(collision.gameObject);
+            money += 1000;
+            UnityEngine.Debug.Log($"Money : {money}");
+        }
+    }
+
 }
